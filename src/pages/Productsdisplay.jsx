6bbea7a6db697715d889f884bb5db3b15e927878
Mahenda reddy <mahendarreddykommidi82@gmail.com>
2025-05-22 21:20:25 +0530

    commits

--- src/pages/Productsdisplay.jsx
+++ src/pages/Productsdisplay.jsx
@@ -10,43 +10,31 @@ const Productsdisplay = ({ product }) => {
   return (
     <div>
       <Navbar />
-      <div className="Productdisplay flex flex-col-reverse gap-8 md:flex-row md:gap-5 my-17 mx-20 px-32">
-        
-              <div className="main-section flex  justify-between gap-15 ">
-        <div className="flex ">
-          <div className="flex flex-col md:flex-row justify-between my-15 gap-15">
-          
+      <div className="Productdisplay flex flex-col md:flex-row items-center  gap-8 md:gap-5 my-10 px-4 md:px-10 lg:px-32">
+        <div className="main-section flex flex-col md:flex-row overflow-hidden gap-8 md:gap-15 w-full">
+          <div className="flex flex-col md:flex-row justify-between my-8 md:my-15 gap-8 md:gap-15 w-full">
+            <img
+              className="w-full md:w-80 max-w-xs md:max-w-65 h-64 md:h-100 object-contain rounded-lg mx-auto "
+              src={product.image}
+              alt=""
+            />
+            <div className="flex flex-col gap-5 w-full md:w-2/3 my-10">
+              <p className="text-gray-600">{product.category}</p>
             
-                       <img className="md:max-w-70 w-full h-100" src={product.image} alt="" />
-
-     
-          <div className="flex flex-col gap-5">
-            <p className="text-gray-600">{product.category}</p>
-             <p className="text-2xl font-medium text-gray-700">{product.title}</p>
-            <p className="max-sm:w-full md:w-full ">{product.description}</p>
-             <div className="flex items-center gap-6">
-            <button className="bg-blue-500 px-6 py-2 text-white rounded-lg">Addtocart</button>
-            <button className="bg-green-500 px-6 py-2 text-white rounded-lg">Buynow</button>
-          </div>
-      
-          </div>
-        
-      
-
-           
-            <div>
-             
+              <p className="text-2xl font-medium text-gray-700">{product.title}</p>
+              <p className="w-full">{product.description}</p>
+              <div className="flex flex-col sm:flex-row items-center gap-4 sm:gap-6">
+                <button className="bg-blue-500 px-6 py-2 text-white rounded-lg w-full sm:w-auto">
+                  Addtocart
+                </button>
+               
+              </div>
             </div>
+            <div></div>
           </div>
         </div>
-         
       </div>
-      </div>
-        {/* Left Section: Image List */}
-        
-  
-    
-<Footer/>
+      <Footer />
     </div>
   );
 };
